2bf1a8949a2547be57b0241260c966cad10a4ec9
Gabriel Cornejo <gabrielcorbot@gmail.com>
2026-01-04 17:39:32 -0500

    refactor custom function logic

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require 'core.options'
 require 'core.keymaps'
+require 'core.custom.smart-comment-enter'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
