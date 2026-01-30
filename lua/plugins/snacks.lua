f36131dc340359bab28c7b4c843ff4d79c4d6cf1
Gabriel Cornejo Botero <60531343+GabrielCornejoB@users.noreply.github.com>
2026-01-29 19:28:51 -0500

    Create snacks.lua

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+-- Tiene muchas vainas pero lo uso como pantalla de bienvenida
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        dashboard = { enabled = true },
+    },
+}
