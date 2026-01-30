78296ac08968d7dbcfbf8ee0493c3bd723304dab
Gabriel Cornejo Botero <60531343+GabrielCornejoB@users.noreply.github.com>
2026-01-29 19:30:25 -0500

    Create which-key.lua

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,7 @@
+-- Menú que muestra opciones disponibles para atajos
+return {
+    'folke/which-key.nvim',
+    commit = '3aab2147e74890957785941f0c1ad87d0a44c15a',
+    event = 'VeryLazy',
+    opts = { }
+}
