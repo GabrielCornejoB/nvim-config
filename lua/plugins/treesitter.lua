144624794be4987ee6b52404c6cb08f53c068a54
Gabriel Cornejo <gabrielcorbot@gmail.com>
2026-02-10 23:52:11 -0500

    fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,6 @@ return {
     commit = 'f8bbc3177d929dc86e272c41cc15219f0a7aa1ac',
     build = 'TSUpdate',
     config = function()
-        --[[
-        --local treesitterConfig = require('nvim-treesitter.config')
-
         require'nvim-treesitter'.install { 
                 "vim", "vimdoc", "lua",
                 "java", "javadoc", "groovy", "properties",
@@ -17,24 +14,5 @@ return {
                 "markdown", "markdown_inline",
                 "dockerfile",
         }
-        --[[
-        treesitterConfig.setup({
-            ensure_installed = { 
-                "vim", "vimdoc", "lua",
-                "java", "javadoc", "groovy", "properties",
-                "javascript", "html", "css", "jsdoc",
-                "typescript", "angular", "scss",
-                "sql",
-                "json", "yaml", "xml",
-                "gitignore", "editorconfig",
-                "markdown", "markdown_inline",
-                "dockerfile",
-            },
-            auto_install = true,
-            highlight = { enable = true },
-            autotag = { enable = true },
-            indent = { enable = true },
-        })
-        ]]--
     end
 }
