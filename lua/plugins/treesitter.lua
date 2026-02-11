efc8ffd7ccb84a044c68bd9718c326ae5303d5b0
Gabriel Sebastián Cornejo Botero <gabriel@MacBook-Air-de-Gabriel.local>
2026-02-10 22:00:43 -0500

    add treesitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,14 +2,23 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     commit = 'f8bbc3177d929dc86e272c41cc15219f0a7aa1ac',
-    dependencies = {
-    },
     build = 'TSUpdate',
     config = function()
         local treesitterConfig = require('nvim-treesitter.config')
 
         treesitterConfig.setup({
-            ensure_installed = { "sql", "vim", "vimdoc", "lua", "java", "javascript", "typescript", "html", "css", "json",  "markdown", "markdown_inline", "gitignore" },
+            ensure_installed = { 
+                "vim", "vimdoc", "lua",
+                "java", "javadoc", "groovy", "properties",
+                "javascript", "html", "css", "jsdoc",
+                "typescript", "angular", "scss",
+                "sql",
+                "json", "yaml", "xml",
+                "gitignore", "editorconfig",
+                "markdown", "markdown_inline",
+                "dockerfile",
+            },
+            auto_install = true,
             highlight = { enable = true },
             autotag = { enable = true },
             indent = { enable = true },
