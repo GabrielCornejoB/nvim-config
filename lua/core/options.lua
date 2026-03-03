a8b990a5e70324fb187352ea880c4974d4d83b7f
Gabriel Cornejo <gabrielcorbot@gmail.com>
2026-03-03 17:37:34 -0500

    docs on vim configs

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,20 +1,20 @@
 -- line numbers
-vim.wo.number = true
-vim.o.relativenumber = true
-vim.o.numberwidth = 2
+vim.wo.number = true -- enable numbers on the left
+vim.o.relativenumber = true -- make numbers on the left change relatively from cursor position
+vim.o.numberwidth = 3 -- make 3 chars the width of the left bar
 vim.o.signcolumn = 'yes'
 
 -- word wrap
-vim.o.wrap = false
+vim.o.wrap = false -- disable word wrap by default
 vim.o.linebreak = true
 
 -- search
-vim.o.ignorecase = true
-vim.o.hlsearch = true
-vim.o.smartcase = true
+vim.o.ignorecase = true -- by default search while ignoring if the text is MAYUS or MINUS
+vim.o.hlsearch = true -- highlight search encounters by default
+vim.o.smartcase = true -- if the pattern contains a character in MAYUS, then it searchs by not ignoring the case
 
 -- clipboard
-vim.o.clipboard = ''
+vim.o.clipboard = vim.fn.has("mac") == 1 and "unnamedplus" or "" -- on windows the clibpard most be used with "+ and then y or p
 
 -- tabs / indentations
 vim.o.shiftwidth = 4
@@ -27,18 +27,17 @@ vim.o.showtabline = 1
 vim.o.autoindent = true
 vim.o.backspace = 'indent,eol,start'
 
--- netrw file explorer
+-- netrw file explorer (disable by default)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- scroll
-vim.o.scrolloff = 8
+vim.o.scrolloff = 8 -- makes it so when scrolling, there are always 8 visible lines
 
--- disable default mode indicator
-vim.o.showmode = false
 
 -- general
 vim.o.fileencoding = 'utf-8'
+vim.o.showmode = false -- disable default show mode, bc lualine plugin already shows it
 vim.opt.termguicolors = true
 vim.opt.backup = false -- disable backup file explorer
 vim.opt.splitbelow = true -- open horizontal splits below current window
@@ -50,3 +49,4 @@ vim.diagnostic.config({
     virtual_text = true, -- inline warnings
     virtual_lines = false, -- newline warnings
 })
+
